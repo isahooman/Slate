e5f706922ed236ecac9822e00cdc9796fd90bbf3
Ng <90578771+isahooman@users.noreply.github.com>
2024-02-04 21:31:33 -0600

    message events:
    - messageDelete
    - messageDeleteBulk
    - messageReactionAdd
    - messageReactionRemoveAll
    - messageReactionRemoveEmoji
    - messageReactionRemove

---
+++ events/messageReactionRemoveAll.js
@@ -0,0 +1,12 @@
+const logger = require('../components/logger.js');
+
+module.exports = {
+  name: 'messageReactionRemoveAll',
+  execute(message) {
+    logger.info(`All reactions removed;
+      Message ID: ${message.id},
+      Channel: ${message.channel.name} | ${message.channel.id},
+      Removed At: ${new Date().toISOString()}
+    `);
+  },
+};
