3f8958a03b0e27bd52104acb2881cd201cc9e178
Ng <90578771+isahooman@users.noreply.github.com>
2024-03-13 15:23:39 -0500

    fixed logs.js
    now sends logs that exceed character limit as a file

--- commands/prefix/owner/logs.js
+++ commands/prefix/owner/logs.js
@@ -1,4 +1,5 @@
 const logger = require('../../../components/logger.js');
+const { AttachmentBuilder } = require('discord.js');
 const path = require('path');
 const fs = require('fs');
 
@@ -13,7 +14,6 @@ module.exports = {
       const linesToRetrieve = parseInt(args[0]);
 
       if (isNaN(linesToRetrieve) || linesToRetrieve <= 0) {
-        // Log invalid input for linesToRetrieve
         logger.debug('[Logs Command] Invalid number of lines requested');
         return message.reply('Please enter a valid number of lines to retrieve.');
       }
@@ -21,11 +21,21 @@ module.exports = {
       // Read the log file
       const logFilePath = path.join(__dirname, '../../../bot.log');
       let logData = fs.readFileSync(logFilePath, 'utf8').trim();
-
       const logLines = logData.split('\n').slice(-linesToRetrieve).join('\n');
 
-      // Send the logs
-      message.reply(`Last ${linesToRetrieve} lines of logs:\n\`\`\`\n${logLines}\n\`\`\``);
+      if (logLines.length > 2000) {
+        // If the log data exceeds 2000 characters, send it as a file
+        const tempFilePath = path.join(__dirname, '../../../tempLog.txt');
+        // Write the logLines to a temporary file
+        fs.writeFileSync(tempFilePath, logLines);
+        const logFile = new AttachmentBuilder(tempFilePath, { name: 'logs.txt' });
+        message.reply({ content: `Here are the last ${linesToRetrieve} lines of logs:`, files: [logFile] })
+          .then(() => fs.unlinkSync(tempFilePath))
+          .catch(error => logger.error(`[Logs Command] Error sending log file: ${error.message}`));
+      } else {
+        // If log data is within the limit, send it as a message
+        message.reply(`Last ${linesToRetrieve} lines of logs:\n\`\`\`\n${logLines}\n\`\`\``);
+      }
       logger.debug('[Logs Command] Successfully replied with logs');
     } catch (error) {
       logger.error(`[Logs Command] An error occurred while reading logs: ${error.message}`);
