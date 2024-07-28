07f57c74e0f636836636f280857c6894f057c471
Ng <90578771+isahooman@users.noreply.github.com>
2024-07-27 23:49:42 -0500

    oops

--- components/loggerUtil.js
+++ components/loggerUtil.js
@@ -4,8 +4,8 @@ let logger = require('./logger.js');
  * Reloads the logger by clearing its cache
  */
 function reloadLogger() {
-  delete require.cache[require.resolve('./loader.js')]; // Clear logger cache
-  logger = require('./loader.js'); // Re-import the logger module
+  delete require.cache[require.resolve('./logger.js')]; // Clear logger cache
+  logger = require('./logger.js'); // Re-import the logger module
 }
 
 module.exports =
