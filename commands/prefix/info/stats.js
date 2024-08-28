120fa7a7ff0d78e04279781d6d22b0aa8e35b39f
Ng <90578771+isahooman@users.noreply.github.com>
2024-08-28 00:28:22 -0500

    stats command cleanup

--- commands/prefix/info/stats.js
+++ commands/prefix/info/stats.js
@@ -13,56 +13,50 @@ module.exports = {
   allowDM: true,
   description: 'Displays bot process statistics',
   execute(message) {
-    // Get memory used by the process
+    // Usage stats
+    const cpuUsage = cpu.usage().then(cpuPercentage => cpuPercentage.toFixed(2));
     const memoryUsage = (process.memoryUsage().heapUsed / 1024 / 1024 / 1024).toFixed(2);
-    logger.debug(`[Stats Command] Memory usage: ${memoryUsage}GB`);
+    const totalSysMem = mem.info().then(info => (info.totalMemMb / 1024).toFixed(2));
+    // Command counts
+    const prefixCommandsCount = client.prefixCommands.size;
+    const slashCommandsCount = client.slashCommands.size;
+    // cache stats
+    const threads = cache.threads.size;
+    const channels = cache.channels.size;
+    const servers = cache.guilds.size;
+    const users = cache.members.size;
+    // Version info
+    const discordJSVersion = require('discord.js').version;
+    const nodeJSVersion = process.version;
+    // uptime
+    const uptime = moment.duration(process.uptime(), 'seconds').format('d[d] h[h] m[m] s[s]');
+    const botUptime = moment.duration(client.uptime, 'milliseconds').format('d[d] h[h] m[m] s[s]');
 
-    // Get total system memory
-    mem.info().then(info => {
-      logger.debug(`[Stats Command] Memory info: ${info.totalMemMb}Mb`);
-      const totalSysMem = (info.totalMemMb / 1024).toFixed(2);
+    // Build stats embed
+    const embed = new EmbedBuilder()
+      .setAuthor({ name: `${client.user.username} stats`, iconURL: client.user.displayAvatarURL() })
+      .setThumbnail(client.user.displayAvatarURL())
+      .addFields(
+        { name: 'CPU Usage', value: `${cpuUsage}%`, inline: true },
+        { name: 'Memory usage', value: `${memoryUsage}GB/${totalSysMem}GB`, inline: true },
+        { name: '‎', value: `‎`, inline: true },
+        { name: 'Prefix Commands', value: `${prefixCommandsCount}`, inline: true },
+        { name: 'Slash Commands', value: `${slashCommandsCount}`, inline: true },
+        { name: '‎', value: `‎`, inline: true },
+        { name: 'Threads', value: `${threads}`, inline: true },
+        { name: 'Channels', value: `${channels}`, inline: true },
+        { name: 'Servers', value: `${servers}`, inline: true },
+        { name: 'Users', value: `${users}`, inline: true },
+        { name: '‎', value: `‎`, inline: true },
+        { name: `Discord.js v${discordJSVersion}`, value: `Nodejs ${nodeJSVersion}` },
+      )
+      .setFooter({ text: `Bot uptime: ${uptime}\nConnection uptime: ${botUptime}` });
 
-      // Get CPU usage
-      cpu.usage().then(cpuPercentage => {
-        logger.debug(`[Stats Command] CPU usage: ${cpuPercentage.toFixed(2)}%`);
-
-        const prefixCommandsCount = client.prefixCommands.size;
-        const slashCommandsCount = client.slashCommands.size;
-        const discordJSVersion = require('discord.js').version;
-        const nodeJSVersion = process.version;
-        const uptime = moment.duration(process.uptime(), 'seconds').format('d[d] h[h] m[m] s[s]');
-        const threads = cache.threads.size;
-        const channels = cache.channels.size;
-        const servers = cache.guilds.size;
-        const users = cache.members.size;
-
-        // Build stats embed
-        const embed = new EmbedBuilder()
-          .setAuthor({ name: `${client.user.username} stats`, iconURL: client.user.displayAvatarURL() })
-          .setThumbnail(client.user.displayAvatarURL())
-          .addFields(
-            { name: 'CPU Usage', value: `${cpuPercentage.toFixed(2)}%`, inline: true },
-            { name: 'Memory usage', value: `${memoryUsage}GB/${totalSysMem}GB`, inline: true },
-            { name: '‎', value: `‎`, inline: true },
-            { name: 'Prefix Commands', value: `${prefixCommandsCount}`, inline: true },
-            { name: 'Slash Commands', value: `${slashCommandsCount}`, inline: true },
-            { name: '‎', value: `‎`, inline: true },
-            { name: 'Threads', value: `${threads}`, inline: true },
-            { name: 'Channels', value: `${channels}`, inline: true },
-            { name: 'Servers', value: `${servers}`, inline: true },
-            { name: 'Users', value: `${users}`, inline: true },
-            { name: '‎', value: `‎`, inline: true },
-            { name: `Discord.js v${discordJSVersion}`, value: `Nodejs ${nodeJSVersion}` },
-          )
-          .setFooter({ text: `Uptime: ${uptime}` });
-
-        try {
-          // Send the embed
-          message.channel.send({ embeds: [embed] });
-        } catch (error) {
-          logger.error(`[Stats Command] Error sending stats embed: ${error.message}`);
-        }
-      });
-    });
+    try {
+      // Send the embed
+      message.channel.send({ embeds: [embed] });
+    } catch (error) {
+      logger.error(`[Stats Command] Error sending stats embed: ${error.message}`);
+    }
   },
 };
