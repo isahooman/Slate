72659212c4f18b1f1e68010d964d4ff9b96fb6d7
Ng <90578771+isahooman@users.noreply.github.com>
2024-11-19 19:17:34 -0600

    botclear amount arg

--- commands/prefix/Owner/botclear.js
+++ commands/prefix/Owner/botclear.js
@@ -1,6 +1,3 @@
-const path = require('path');
-const { readFile } = require('../../../components/fileHandler.js');
-const config = readFile(path.join(__dirname, '../../../config/config.json5'));
 const logger = require('../../../components/logger.js');
 
 const prefixes = ['\'', '$', ',', '-', 't!', 't@', '!', '+', '_', ';', '.', '?', 's?', 'p!', 'r.', 'do.', 0,
@@ -10,48 +7,39 @@ const prefixes = ['\'', '$', ',', '-', 't!', 't@', '!', '+', '_', ';', '.', '?',
 
 module.exports = {
   name: 'botclear',
-  usage: 'bc <self/all>',
+  usage: 'bc [number of messages]',
   category: 'Owner',
   aliases: ['bc'],
   allowDM: false,
   description: 'Clears bot messages',
   execute: async(message, args) => {
-    // Check the arg provided
-    const scope = args[0] || 'all';
-    logger.debug(`[BotClear Command] Scope determined: ${scope}`);
-
     try {
-      // Fetch the last 100 messages in the channel
-      const fetchedMessages = await message.channel.messages.fetch({ limit: 100 });
+      // Get the number of messages to scan from args, default to 30
+      const messagesToScan = parseInt(args[0]) || 30;
+      logger.debug(`[BotClear Command] Messages to scan: ${messagesToScan}`);
+
+      // Fetch the given number of messages
+      const fetchedMessages = await message.channel.messages.fetch({ limit: messagesToScan });
       logger.debug(`[BotClear Command] Messages Fetched: ${fetchedMessages.size}`);
 
-      let deletableMessages;
-
-      // Filter messages to be deleted
-      if (scope === 'self') deletableMessages = fetchedMessages.filter(msg =>
-        msg.author.id === message.client.user.id || msg.content.startsWith(config.prefix),
-      );
-      else deletableMessages = fetchedMessages.filter(m =>
+      // Filter bot messages
+      const deletableMessages = fetchedMessages.filter(m =>
         m.author.bot || prefixes.some(prefix => m.content.startsWith(prefix)),
       );
 
       logger.debug(`[BotClear Command] Deletable messages: ${deletableMessages.size}`);
 
-      // Filter messages to messages from the last 10 minutes
-      const messagesToDelete = deletableMessages.filter(msg => msg.createdTimestamp > (Date.now() - 10 * 60 * 1000));
-      logger.debug(`[BotClear Command] Messages to delete: ${messagesToDelete.size}`);
-
       // Bulk delete the filtered messages
-      await message.channel.bulkDelete(messagesToDelete, true);
+      await message.channel.bulkDelete(deletableMessages, true);
       logger.debug(`[BotClear Command] Bulk deleted messages`);
 
       // Send a confirmation message and delete it after 3 seconds
-      const confirmationMessage = await message.channel.send(`Cleared ${messagesToDelete.size} messages.`);
+      const confirmationMessage = await message.channel.send(`Cleared ${deletableMessages.size} messages.`);
       setTimeout(() => {
         confirmationMessage.delete().catch(e => {
           throw Error(`Error deleting confirmation message: ${e.message}`);
         });
-      }, 3000);
+      }, 5000);
     } catch (error) {
       throw Error(`Error executing bc command: ${error.message}`);
     }
