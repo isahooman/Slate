f43421f3afb37574b41d66c7bd8da3feeab3fceb
Ng <90578771+isahooman@users.noreply.github.com>
2025-05-08 00:36:11 -0500

    Fix channelPinsUpdate event

--- bot/events/channel/channelPinsUpdate.js
+++ bot/events/channel/channelPinsUpdate.js
@@ -3,9 +3,11 @@ const logger = require('../../components/util/logger.js');
 module.exports = {
   name: 'channelPinsUpdate',
   execute(channel, time) {
+    const readableTime = time ? new Date(time).toLocaleString() : 'Not available';
+
     logger.info(`Channel pins updated;
       Channel: ${channel.name} | ${channel.id},
-      Last Pins Update: ${time.toISOString()},
+      Last Pins Update: ${readableTime},
     `);
   },
 };
