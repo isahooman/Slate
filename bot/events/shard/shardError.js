7612567a8f0ffb402f28ba447824e80f36b250bc
Ng <90578771+isahooman@users.noreply.github.com>
2025-06-17 16:33:35 -0500

    ignore network errors

--- bot/events/shard/shardError.js
+++ bot/events/shard/shardError.js
@@ -3,6 +3,10 @@ const logger = require('../../components/util/logger.js');
 module.exports = {
   name: 'shardError',
   execute(error, shardId) {
+    // Ignore network errors
+    if (error.message && error.message.includes('getaddrinfo ENOTFOUND')) return;
+
+    // Log other shard errors
     logger.error(`Error on shard ${shardId};
       Message: ${error.message},
       Stack Trace: ${error.stack || 'N/A'}
